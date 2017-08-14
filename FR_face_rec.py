5e7ed7ebc24a2b7f9853d3e08bd4739bcf9515a0
zts <hquzts@163.com>
2017-08-14 14:37:13 +0800

    fist

--- FR_face_rec.py
+++ FR_face_rec.py
@@ -65,12 +65,6 @@ while True:
 
         # Display the results
         for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            # top *= 4
-            # right *= 4
-            # bottom *= 4
-            # left *= 4
-
             # Draw a box around the face
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
